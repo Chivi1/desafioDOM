f9eb8b71b36574e118ad8e9ffcaaab9d7b00481b
chivi <paulofr016@gmail.com>
2022-08-05 13:30:23 -0300

    email js añadido

--- main.js
+++ main.js
@@ -78,39 +78,8 @@ formulario.addEventListener("submit", (e) => {
             console.log(data);
             console.log(datos.get("correo"));
             });
-            //nodemailer para enviar mail de bienvenida a correo de input
+            //mail API para enviar mensaje de bienvenida al correo electronico de input
         let mailUsuario =  datos.get("correo");
-        const nodemailer = require("nodemailer");
-            async function main() {
-            // Generate test SMTP service account from ethereal.email
-            // Only needed if you don't have a real mail account for testing
-            let testAccount = await nodemailer.createTestAccount();
-            // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-        },
-    });
-        // send mail with defined transport object
-            let info = await transporter.sendMail({
-            from: '"Blacktail" <blacktailesports@gmail.com>', // sender address
-            to: mailUsuario , // list of receivers
-            subject: "Bienvenido", // Subject line
-            text: "BIENVENIDO A BLACKTAIL", // plain text body
-             html: "<h1>Bienvenido a Blacktail Esports, estamos muy felices de que quieras formar parte de nuestro equipo, ¡estaremos revisando tus datos y nos comunicaremos contigo a la brevedad!</h1>", // html body
-            });
-        console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com> 
-        // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    }
-    main().catch(console.error);
-        
-    exit();
+        exit();
 }); 
 
